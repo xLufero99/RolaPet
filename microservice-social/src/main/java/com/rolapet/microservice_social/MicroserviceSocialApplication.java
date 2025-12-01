f4cd8cc349b845c9e55ae9101c3cc903ab9992d3
xLufero99 <enanitogomez2011@gmail.com>
2025-11-30 23:01:33 -0500

    se crea la estructura de el service social

---
+++ microservice-social/src/main/java/com/rolapet/microservice_social/MicroserviceSocialApplication.java
@@ -0,0 +1,13 @@
+package com.rolapet.microservice_social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSocialApplication.class, args);
+	}
+
+}
---
+++ microservice-social/src/test/java/com/rolapet/microservice_social/MicroserviceSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolapet.microservice_social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
